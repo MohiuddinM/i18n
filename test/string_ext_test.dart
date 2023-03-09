906850f2ad0db8e3591a63e435ef167412694210
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2023-03-09 19:22:37 +0100

    add tests

--- test/string_ext_test.dart
+++ test/string_ext_test.dart
@@ -2,6 +2,18 @@ import 'package:i18n/src/string_ext.dart';
 import 'package:test/test.dart';
 
 void main() => group('StringX', () {
+      test('convert name removes spaces', () {
+        expect('user string'.convertName(), 'userString');
+        expect('user string (int cnt)'.convertName(), 'userString(int cnt)');
+        expect('user string(int cnt)'.convertName(), 'userString(int cnt)');
+      });
+
+      test('convert name removes hyphens', () {
+        expect('user-string'.convertName(), 'userString');
+        expect('user-string (int cnt)'.convertName(), 'userString(int cnt)');
+        expect('user-string(int cnt)'.convertName(), 'userString(int cnt)');
+      });
+
       test('firstUpper should convert first character to uppercase', () {
         expect('string'.firstUpper(), 'String');
         expect('1string'.firstUpper(), '1string');
@@ -13,29 +25,29 @@ void main() => group('StringX', () {
       });
 
       test('containsReference return true if there is a reference', () {
-        expect(r'$name'.containsReference, isTrue);
-        expect(r'\$name'.containsReference, isFalse);
-        expect(r'name'.containsReference, isFalse);
-        expect(r'300\$'.containsReference, isFalse);
-        expect(() => r'_\$$name'.containsReference, throwsArgumentError);
-      });
+    expect(r'$name'.containsReference, isTrue);
+    expect(r'\$name'.containsReference, isFalse);
+    expect(r'name'.containsReference, isFalse);
+    expect(r'300\$'.containsReference, isFalse);
+    expect(() => r'_\$$name'.containsReference, throwsArgumentError);
+  });
 
-      test('convert name works if there are no _ in the name', () {
-        expect('message'.convertName(), 'message');
-      });
+  test('convert name works if there are no _ in the name', () {
+    expect('message'.convertName(), 'message');
+  });
 
-      test('convert name works if there is 1 _ in the name', () {
-        expect('message_de'.convertName(), 'messageDe');
-      });
+  test('convert name works if there is 1 _ in the name', () {
+    expect('message_de'.convertName(), 'messageDe');
+  });
 
-      test('convert name works if there are 2 _ in the name', () {
-        expect('message_en_us'.convertName(), 'messageEnUs');
-      });
+  test('convert name works if there are 2 _ in the name', () {
+    expect('message_en_us'.convertName(), 'messageEnUs');
+  });
 
-      test('convert name works if there are many _ in the name', () {
-        expect(
+  test('convert name works if there are many _ in the name', () {
+    expect(
           () => 'translation_message_en_us'.convertName(),
-          throwsArgumentError,
-        );
-      });
+      throwsArgumentError,
+    );
+  });
     });
