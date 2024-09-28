a4e717a1c7e1bcf719914338ec4d33f5650e372f
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2024-09-28 17:55:02 +0200

    make const

--- lib/src/metadata.dart
+++ lib/src/metadata.dart
@@ -9,7 +9,7 @@ class Metadata {
   final String localeName;
   final String languageCode;
 
-  Metadata({
+  const Metadata({
     this.parent,
     required this.isDefault,
     required this.defaultObjectName,
@@ -30,11 +30,55 @@ class Metadata {
       languageCode: languageCode,
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Metadata &&
+          runtimeType == other.runtimeType &&
+          parent == other.parent &&
+          isDefault == other.isDefault &&
+          defaultObjectName == other.defaultObjectName &&
+          defaultFileName == other.defaultFileName &&
+          objectName == other.objectName &&
+          localeName == other.localeName &&
+          languageCode == other.languageCode;
+
+  @override
+  int get hashCode =>
+      parent.hashCode ^
+      isDefault.hashCode ^
+      defaultObjectName.hashCode ^
+      defaultFileName.hashCode ^
+      objectName.hashCode ^
+      localeName.hashCode ^
+      languageCode.hashCode;
+
+  @override
+  String toString() {
+    return 'Metadata(parent: $parent, isDefault: $isDefault, defaultObjectName: $defaultObjectName, defaultFileName: $defaultFileName, objectName: $objectName, localeName: $localeName, languageCode: $languageCode)';
+  }
 }
 
 class Translation {
   final Metadata metadata;
   final YamlMap content;
 
-  Translation(this.metadata, this.content);
+  const Translation(this.metadata, this.content);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Translation &&
+          runtimeType == other.runtimeType &&
+          metadata == other.metadata &&
+          content == other.content;
+
+  @override
+  int get hashCode => metadata.hashCode ^ content.hashCode;
+
+  @override
+  String toString() {
+    return 'Translation(metadata: $metadata, content: $content)';
+  }
 }
